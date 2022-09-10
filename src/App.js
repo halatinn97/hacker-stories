6e64acc57f37283b93bb80017eca6bf18fcf7473
Hala Tinn <hala79tinn@gmail.com>
2022-09-10 20:09:52 +0200

    Render example articles

--- src/App.js
+++ src/App.js
@@ -11,6 +11,24 @@ function searchPage(search) {
   return search;
 }
 
+const list = [
+  {
+    listTitle: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    listTitle: 'Redux',
+    url: 'https://redux.js/org',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+];
 
 function App() {
 
@@ -21,6 +39,28 @@ function App() {
       <label htmlFor="search">Search:  </label>
       <input id="search" type="text"></input>
       <button>{searchPage('Find')}</button>
+
+      <ul>
+        <h3>Article Summary</h3>
+        {list.map(function (item) {
+          return (
+            <>
+              <li key={item.objectID}>
+                <span>
+                  <a href={item.url}>{item.listTitle}</a>
+                </span>
+                <br></br>
+                <span>Author: {item.author}</span>
+                <br></br>
+                <span># of Comments: {item.num_comments}</span>
+                <br></br>
+                <span>Rating: {item.points}</span>
+              </li>
+            </>
+          );
+        })}
+      </ul>
+
     </div>
   );
 }
